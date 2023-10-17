babda46508373799df8d2bd90acdb5cc7dc7f838
babs3 <barbaraema3@gmail.com>
2023-10-17 18:45:14 +0100

    simplified version to start thinking in something

---
+++ simplified_version/README.md
@@ -0,0 +1,6 @@
+## My LIDL
+
+- This simplified version demonstrates the essential components for creating and managing shopping lists locally and syncing them with a cloud component.
+
+- There is an Example usage in main.py. To run the code write the following command on the terminal in this folder:
+\- `python main.py`
---
+++ simplified_version/models.py
@@ -0,0 +1,32 @@
+import uuid
+
+class ShoppingListItem: # represents an item in a shopping list
+    def __init__(self, name, quantity, acquired=False):
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.quantity = quantity
+        self.acquired = acquired # whether the item is acquired or not
+
+    def __str__(self):
+        # string representation of the item for printing purposes
+        return f"Item ID: {self.id}, Name: {self.name}, Quantity: {self.quantity}, Acquired: {self.acquired}"
+
+
+class ShoppingList: # represents a shopping list that contains multiple items
+    def __init__(self, list_id):
+        self.list_id = list_id  # unique identifier for the shopping list
+        self.items = []         # a list to store ShoppingListItem objects
+
+    def add_item(self, name, quantity):
+        item = ShoppingListItem(name, quantity)
+        self.items.append(item)
+
+    def delete_item(self, item_id):
+        self.items = [item for item in self.items if item.id != item_id]
+
+    def acquire_item(self, item_id):
+        for item in self.items:
+            if item.id == item_id:
+                item.acquired = True
+                if item.quantity > 0:
+                    item.quantity -= 1
