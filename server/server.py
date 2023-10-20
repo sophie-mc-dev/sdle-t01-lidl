256a155c13c3ffc1635b7a06a4ef36eafd456d17
babs3 <barbaraema3@gmail.com>
2023-10-20 17:28:00 +0100

    server handle multiple clients

--- server/server.py
+++ server/server.py
@@ -1,7 +1,5 @@
 import socket
-from importlib.machinery import SourceFileLoader
-
-
+import threading
 from operations import *
 
 # Create a socket
@@ -18,67 +16,55 @@ server_socket.bind((host, port))
 server_socket.listen(5)  # 5 connections for now
 print("Server is listening...")
 
-
 # CREATE LIST
-
 list_id = create_shopping_list()
 add_item_to_list(list_id, "Milk", 2)
 add_item_to_list(list_id, "Eggs", 1)
 
-while True:
-    # Accept a connection from a client
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
+# Function to handle a client
+def handle_client(client_socket):
+
+    print(f"Connection from {client_socket.getpeername()}")
 
     while True:
-        client_socket.send("\nPress 1 to see list, 2 to add element:".encode())
-
-        # Receive the key from the client
-
-        try:
-            key = client_socket.recv(1024).decode().strip()
-        except ConnectionResetError:
-            print("Client closed the connection.")
-            break
+        client_socket.send("\nPress 1 to see list, 2 to add element, or 0 to exit:".encode())
+        key = client_socket.recv(1024).decode().strip()
 
         if not key:
             print("Client disconnected unexpectedly.")
             break
-        
-        print(f"Received key: {key}")
 
-        
-        if (key == "1"):
-
-            # Get the list items
+        if key == "1":
             items = [str(item) for item in local_lists[list_id].items]
-
-            # Send the list items back to the client
             client_socket.send("\n".join(items).encode())
 
-
-        elif (key == "2"):
+        elif key == "2":
 
             client_socket.send("Name of the item:".encode())
-
             name = client_socket.recv(1024).decode().strip()
 
             client_socket.send("Quantity:".encode())
-
             quantity = client_socket.recv(1024).decode().strip()
 
             try:
                 quantity = int(quantity)
                 add_item_to_list(list_id, name, quantity)
-
             except ValueError:
                 client_socket.send("Invalid quantity. Please enter a valid integer.".encode())
 
-        elif (key == "0"):
-            break # break inner loop
-
+        elif key == "0":
+            break
+        
         else:
             client_socket.send("Invalid key".encode())
 
-    # Close the client socket
     client_socket.close()
+
+
+# Main server loop
+while True:
+    client_socket, client_address = server_socket.accept()
+    
+    # Create a new thread to handle the client
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
