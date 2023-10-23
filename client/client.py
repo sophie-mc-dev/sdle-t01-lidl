b9f1cf0a57551b6c2d6adf3ade5f247c21130bee
babs3 <barbaraema3@gmail.com>
2023-10-23 21:43:16 +0100

    added simple auhentication mechanism

--- client/client.py
+++ client/client.py
@@ -10,17 +10,37 @@ port = 5555
 # Connect to the server
 client_socket.connect((host, port))
 
+authenticated = False
 
+while not authenticated:
+    # Authentication loop
+    message = client_socket.recv(1024).decode()
+    #print(message)
+
+    if "Username" in message:
+        username = input("Username: ")
+        client_socket.send(username.encode())
+
+    elif "Password" in message:
+        password = input("Password: ")
+        client_socket.send(password.encode())
+
+    elif "Authentication successful" in message:
+        authenticated = True
+
+    else:
+        print("Authentication failed. Please try again.")
+
+
+# Continue with list management or other operations
 while True:
     # Receive and print the server's message
     message = client_socket.recv(1024).decode()
     print(message)
 
-    if message.endswith(":"): 
-        key = (input())
+    if message.endswith(":"):
+        key = input()
         client_socket.send(key.encode())
-
-
 
 
 # Close the client socket
--- server/server.py
+++ server/server.py
@@ -16,6 +16,12 @@ server_socket.bind((host, port))
 server_socket.listen(5)  # 5 connections for now
 print("Server is listening...")
 
+# Dictionary to store username-password mappings (for illustration purposes)
+user_credentials = {
+    "user1": "password1",
+    "user2": "password2",
+}
+
 # CREATE LIST
 list_id = create_shopping_list()
 add_item_to_list(list_id, "Milk", 2)
@@ -23,9 +29,25 @@ add_item_to_list(list_id, "Eggs", 1)
 
 # Function to handle a client
 def handle_client(client_socket):
-
     print(f"Connection from {client_socket.getpeername()}")
 
+    authenticated = False  # Track authentication status
+
+    while not authenticated:
+        # Authentication loop
+        client_socket.send("Username:".encode())
+        username = client_socket.recv(1024).decode().strip()
+
+        client_socket.send("Password:".encode())
+        password = client_socket.recv(1024).decode().strip()
+
+        if username in user_credentials and user_credentials[username] == password:
+            authenticated = True
+            client_socket.send("Authentication successful. You can now access your list.".encode())
+        else:
+            client_socket.send("Authentication failed. Please try again.".encode())
+
+    # Once authenticated, proceed with list management
     while True:
         client_socket.send("\nPress 1 to see list, 2 to add element, or 0 to exit:".encode())
         key = client_socket.recv(1024).decode().strip()
@@ -39,7 +61,6 @@ def handle_client(client_socket):
             client_socket.send("\n".join(items).encode())
 
         elif key == "2":
-
             client_socket.send("Name of the item:".encode())
             name = client_socket.recv(1024).decode().strip()
 
@@ -54,17 +75,14 @@ def handle_client(client_socket):
 
         elif key == "0":
             break
-        
-        else:
-            client_socket.send("Invalid key".encode())
 
     client_socket.close()
-
 
 # Main server loop
 while True:
     client_socket, client_address = server_socket.accept()
-    
+
     # Create a new thread to handle the client
     client_handler = threading.Thread(target=handle_client, args=(client_socket,))
     client_handler.start()
+
