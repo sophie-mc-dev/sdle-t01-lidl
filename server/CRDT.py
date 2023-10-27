8dd8788fd45e618f0dd7921e4256255165f8c7b2
Sofia Costa <up202300565@edu.fe.up.pt>
2023-10-27 12:35:15 +0100

    update CRDT

--- server/CRDT.py
+++ server/CRDT.py
@@ -1,11 +1,15 @@
-class CRDTShoppingList: # represents a shopping list that contains multiple items
+class CRDTShoppingList: 
 
     def __init__(self, unique_id): 
-        self.unique_id = unique_id # unique identifier for the shopping list (URL format)
-        self.items = []  # a list to store ShoppingListItem objects
+        self.unique_id = unique_id 
+        self.items = []  
 
     # add item to list
-    def add_item(self, item_name, quantity=1):
+    def add_item(self, item_name, quantity, acquired):
+        # initialize
+        quantity = 1
+        acquired = False
+
         # Check if the item with the same name already exists in the list
         for i, (existing_item, existing_quantity) in enumerate(self.items):
             if existing_item == item_name:
@@ -16,27 +20,38 @@ class CRDTShoppingList: # represents a shopping list that contains multiple item
         # If the item doesn't exist, add it to the list with the specified quantity
         if quantity < 1:
             raise ValueError("Quantity must be a positive integer.")
-        self.items.append((item_name, quantity))
+        self.items.append({'name': item_name, 'quantity': quantity, 'acquired': acquired})
 
     # remove item from list
-    def remove_item(self, item_name, quantity):
-        if quantity < 1:
-            raise ValueError("Quantity must be a positive integer.")
-        
-        updated_items = []
-        for item, existing_quantity in self.items:
-            if item == item_name:
+    def remove_item(self, item_name):
+        self.items = [item for item in self.items if item != item_name]
+
+    def increase_quantity(self, item_name, quantity=1):
+        for i, (existing_item, existing_quantity) in enumerate(self.items):
+            if existing_item == item_name:
+                self.items[i] = (existing_item, existing_quantity + quantity)
+                return
+            
+    def decrease_quantity(self, item_name, quantity=1):
+        for i, (existing_item, existing_quantity) in enumerate(self.items):
+            if existing_item == item_name:
                 remaining_quantity = existing_quantity - quantity
-                if remaining_quantity > 0:
-                    updated_items.append((item, remaining_quantity))
-            else:
-                updated_items.append((item, existing_quantity))
-        
-        self.items = updated_items
+                if remaining_quantity >= 1:
+                    self.items[i] = (existing_item, remaining_quantity)
+                else:
+                    self.remove_item(item_name)
+
+    # add function that marks item as acquired
+    def acquire_item(self, item_name):
+        for i, (existing_item, existing_quantity) in enumerate(self.items):
+            if existing_item == item_name:
+                self.items[i] = (existing_item, existing_quantity, True)
 
     # merge items
     def merge(self, other):
         self.items = self.data.union(other.items)
+
+    # implement a conflict resolution strategy
 
     # display shopping list
     def get_list(self):
