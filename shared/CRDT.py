75f9376521aef6841e34935b78175e06da434f9a
Sofia Merino Costa <scosta.geprix@gmail.com>
2023-11-28 18:54:25 +0000

    test merge function with threading [still in the works]

--- shared/CRDT.py
+++ shared/CRDT.py
@@ -153,20 +153,83 @@ class ShoppingList:
                 self.acquired_counters[item_id].inc(list_id) if status else self.acquired_counters[item_id].dec(list_id)
 
     def merge(self, other_map):
-        for item_id, other_item in other_map.shopping_map.items():
-            if item_id in self.Items:
-                current_item = self.Items[item_id]
-                if other_item["timestamp"] > current_item["timestamp"]:
+        # Extract item IDs from the current instance and the other_map
+        self_item_ids = set(self.Items.keys())
+        other_item_ids = set(other_map.Items.keys())
+
+        # Determine common items between the two sets
+        common_items = self_item_ids.intersection(other_item_ids)
+
+        # Handle conflicts based on timestamps and acquired status
+        for item_id in common_items:
+            self_item = self.Items[item_id]
+            other_item = other_map.Items[item_id]
+
+            if other_item["timestamp"] > self_item["timestamp"]:
+                # Implement conflict resolution strategies
+                if other_item["timestamp"] == self_item["timestamp"]:
+                    if other_item["acquired"] and not self_item["acquired"]:
+                        self.Items[item_id] = other_item  # Update with the latest timestamp
+                else:
                     self.Items[item_id] = other_item  # Update with the latest timestamp
-            else:
-                self.Items[item_id] = other_item  # Add if the key doesn't exist in self.shopping_map
 
-            if item_id in other_map.quantity_counters:
-                if item_id not in self.quantity_counters:
-                    self.quantity_counters[item_id] = PNCounter()
-                self.quantity_counters[item_id].merge(other_map.quantity_counters[item_id])
+        # Merge items from other_map into self.Items
+        for item_id in other_item_ids:
+            if item_id not in self_item_ids:
+                self.Items[item_id] = other_map.Items[item_id]
 
-            if item_id in other_map.acquired_counters:
-                if item_id not in self.acquired_counters:
-                    self.acquired_counters[item_id] = PNCounter()
-                self.acquired_counters[item_id].merge(other_map.acquired_counters[item_id])
+        # Merge quantity counters and acquired counters as before
+        if item_id in other_map.quantity_counters:
+            if item_id not in self.quantity_counters:
+                self.quantity_counters[item_id] = PNCounter(item_id)
+            self.quantity_counters[item_id].merge(other_map.quantity_counters[item_id])
+
+        if item_id in other_map.acquired_counters:
+            if item_id not in self.acquired_counters:
+                self.acquired_counters[item_id] = PNCounter(item_id)
+            self.acquired_counters[item_id].merge(other_map.acquired_counters[item_id])
+
+       
+
+
+import time
+from threading import Thread
+
+# Function to simulate updates on replica 1
+def simulate_replica1_updates(replica1):
+    for i in range(5):
+        item_id = str(i)
+        item = {"name": f"Item_{i}", "quantity": i, "acquired": False}
+        replica1.add_item(item_id, item)
+        time.sleep(0.5)  # Introduce delay between updates
+
+# Function to simulate conflicting updates on replica 2
+def simulate_replica2_updates(replica2):
+    for i in range(5):
+        item_id = str(i)
+        # Simulate conflicting updates with different values but same key and timestamp
+        item = {"name": f"Conflicting_Item_{i}", "quantity": i + 10, "acquired": True}
+        replica2.add_item(item_id, item)
+        time.sleep(0.5)  # Introduce delay between updates
+
+# Create instances of ShoppingList to simulate different replicas
+replica1 = ShoppingList()
+replica2 = ShoppingList()
+
+# Start threads to simulate updates on replicas concurrently
+thread_replica1 = Thread(target=simulate_replica1_updates, args=(replica1,))
+thread_replica2 = Thread(target=simulate_replica2_updates, args=(replica2,))
+
+thread_replica1.start()
+thread_replica2.start()
+
+# Wait for threads to finish
+thread_replica1.join()
+thread_replica2.join()
+
+# Merge the updates from replica 2 into replica 1
+replica1.merge(replica2)
+
+# Get the final shopping list state from replica 1 after merging
+print("Final Shopping List State from Replica 1 after merging:")
+print(replica1.get_shopping_list(replica1.my_id()))  # You might need to provide the shopping list ID or adjust the method if required
